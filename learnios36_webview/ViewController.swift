87f29bca250313f3275122d78f4bc869e5d0ab0c
Jung No <jungno@gmail.com>
2015-09-17 19:33:48 -0700

    tested text input http://entersomething, back, forward

--- learnios36_webview/ViewController.swift
+++ learnios36_webview/ViewController.swift
@@ -9,16 +9,64 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var addressBar: UITextField!
+    @IBOutlet weak var infoLabel: UILabel!
+    @IBOutlet weak var webView: UIWebView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    @IBAction func gobackButton(sender: UIButton) {
+        print("yo goback pressed")
+        webView.goBack()
+    }
+    
+    
+    @IBAction func goButton(sender: UIButton) {
+        print("yo gobutton pressed")
+        if addressBar.text == "" {
+            return 
+        }
+        
+        guard let text: String = addressBar.text else {
+            return
+        }
+        loadURL(text)
+        
+    }
+    
+    
+    
+    //takes a string and load url
+    func loadURL(str:String) {
+        print("yo loadURL")
+        let url = NSURL(string: str)!
+        let request = NSURLRequest(URL: url)
+        webView.loadRequest(request)
+        
+        
+    }
+    
+    
+    @IBAction func goforwardButton(sender: UIButton) {
+        print("yo goforward pressed")
+        webView.goForward()
+    }
+    
+    
 
 
 }
